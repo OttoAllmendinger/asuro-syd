38b549407d9042fad2bdbb7c78c3b5b55c0d8b1e
atlas@zappa <none@none>
2009-06-30 07:21:16 +0200

    test commit

--- lib/syd/syd_util.c
+++ lib/syd/syd_util.c
@@ -3,6 +3,9 @@
 #include "syd.h"
 #include "syd_util.h"
 
+
+// TODO: reintegrate ser_printf? 
+
 void send(uchar id, uchar* data, char type, uchar size) {
     char HEADER[] = {'\n', 's', 'e', 'n', 'd', id, type, size };
     SerWrite(HEADER, 8);
