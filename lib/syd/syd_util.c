a2f1e3b10d05e1c57ab2d88ebd31e3419e1575fe
otto.allmendinger@googlemail.com <none@none>
2009-07-03 09:08:59 +0200

    - reimplemented ser_printf

--- lib/syd/syd_util.c
+++ lib/syd/syd_util.c
@@ -1,3 +1,9 @@
+#ifdef SERPRINT
+#include "stdarg.h"
+#include "stdlib.h"
+#include "string.h"
+#endif
+
 #include "asuro.h"
 
 #include "syd.h"
@@ -5,6 +11,21 @@
 
 
 // TODO: reintegrate ser_printf? 
+
+#ifdef SERPRINT
+void ser_printf(const char *fmt, ...) {
+    va_list args;
+    uchar STATUS[SIZE_STATUS];
+    va_start(args, fmt);
+    uchar s = (uchar) vsnprintf(STATUS, SIZE_STATUS, fmt, args);
+    va_end(args);
+    SerWrite(STATUS, s);
+}
+#else
+void ser_printf(const char *fmt, ...) {
+    // do nuthing
+};
+#endif
 
 void send(uchar id, uchar* data, char type, uchar size) {
     char HEADER[] = {'\n', 's', 'e', 'n', 'd', id, type, size };
